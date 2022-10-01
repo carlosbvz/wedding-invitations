eda6fc274fe2f6e5e9d9659e2572a6ff0b3a6789
Carlos Benavides <carlosbvz@gmail.com>
2022-10-01 11:16:13 -0600

    add fire img

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,8 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
-
-import env2 from "../public/images/env3.png";
-import env3 from "../public/images/env4.png";
 import env5 from "../public/images/env5.png";
+import fire from "../public/images/fire.png";
 
 const Home: NextPage = () => {
   return (
@@ -19,15 +17,10 @@ const Home: NextPage = () => {
           {" "}
           {"It's dangerous to go alone! Take this."}
         </h1>
-        <Image width={120} height={80} src={env2} />
-        <br />
-        <br />
-        <Image width={120} height={80} src={env3} />
-        <br />
-        <br />
         <Image width={120} height={80} src={env5} />
         <br />
         <br />
+        <Image width={120} height={80} src={fire} />
       </main>
     </div>
   );
