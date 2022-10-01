ab57333992721029e93b701cf3c05405fd23ffd0
Carlos Benavides <carlosbvz@gmail.com>
2022-10-01 12:43:46 -0600

    animation added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,35 @@
+import { useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
 import env5 from "../public/images/env5.png";
 import fire from "../public/images/fire.png";
+
+import { animated, useSpring } from "@react-spring/web";
+
+function FadeIn({
+  children,
+  customProps,
+}: {
+  children: any;
+  customProps?: any;
+}) {
+  const [flip, set] = useState(false);
+  const props = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    reset: true,
+    reverse: flip,
+    ...customProps,
+  });
+
+  return (
+    <animated.div style={props} className={styles.title}>
+      {children}
+    </animated.div>
+  );
+}
 
 const Home: NextPage = () => {
   return (
@@ -13,16 +39,24 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          {" "}
-          {"It's dangerous to go alone! Take this."}
-        </h1>
+        <FadeIn customProps={{ config: { duration: 4000 } }}>
+          <h1 className={styles.title}>
+            {"It's dangerous to go alone! Take this."}
+          </h1>
+        </FadeIn>
+
         {/* Fire */}
-        <div className={styles.firecontainer}>
-          <Image width={120} height={80} src={fire} className={styles.fire} />
-          <Image width={120} height={80} src={fire} className={styles.fire} />
-        </div>
-        <Image width={120} height={80} src={env5} />
+        <FadeIn customProps={{ config: { duration: 4000 } }}>
+          <div className={styles.firecontainer}>
+            <Image width={120} height={80} src={fire} className={styles.fire} />
+            <Image width={120} height={80} src={fire} className={styles.fire} />
+          </div>
+        </FadeIn>
+        <br />
+        <br />
+        <FadeIn customProps={{ delay: 4000, config: { duration: 4000 } }}>
+          <Image width={120} height={80} src={env5} />
+        </FadeIn>
         <br />
         <br />
       </main>
