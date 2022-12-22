11604cba0826b67e98a969407d6b64d0bcae60d9
Carlos Benavides <carlosbvz@gmail.com>
2022-12-22 13:24:15 -0600

    update awsmobile

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -7,7 +7,7 @@ export default function Info({}: Props) {
     <CenterContainer style={{ marginTop: "100px" }}>
       <h1 style={{ color: "white" }}>Carlos Y Paola</h1>
       <p style={{ color: "white" }}>Te invitamos a nuestra boda</p>
-      <p style={{ color: "white" }}>Sabado 13 de Enero, 2023</p>
+      <p style={{ color: "white" }}>Sabado 21 de Enero, 2023</p>
       <p style={{ color: "white" }}>4:00 pm</p>
 
       <br />
