919e13cf03eb1deef26481d2e7e80e50a6e00883
Carlos Benavides <carlosbvz@gmail.com>
2022-12-22 13:04:20 -0600

    update awsmobile

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,16 @@ import Head from "next/head";
 import awsconfig from "../aws-exports";
 import GlobalProvider from "../contexts/GlobalContext";
 
-Amplify.configure({ ...awsconfig, ssr: true });
+const awsmobile = {
+  aws_project_region: "us-east-1",
+  aws_appsync_graphqlEndpoint:
+    "https://v72qg3emvnh57eiof6ytacovae.appsync-api.us-east-1.amazonaws.com/graphql",
+  aws_appsync_region: "us-east-1",
+  aws_appsync_authenticationType: "API_KEY",
+  aws_appsync_apiKey: "da2-3nmippjvxfcmzgezejbr3rfciy",
+};
+
+Amplify.configure({ ...awsmobile, ssr: true });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
